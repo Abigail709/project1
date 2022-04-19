e3e879af9c0b865f8169ac4205cee8e5435288dd
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-04-19 12:58:51 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Compatibile or Not? Find true love here.
+UT Bootcamp Collaborative Project 01
+
+## Collaborators:
+Timothy Zalewski: https://github.com/Tim-Zebra <br />
+Jace Feinstein: https://github.com/jacef790 <br />
+Abigail Asaju: https://github.com/Abigail709 <br />
+Robert Leland: https://github.com/RLeland12
+
+## Description:
+A web application that stream lines the dating process by finding love based only on two names!
+
+## How to Use:
+To obtain compatibility: Enter your name and their name, then click 'Compare!' <br />
+To obtain jokes: Select the check boxes you want for your search criteria. The click 'Submit Criteria!' to obtain jokes. <br />
+To obtain an activity: Select 'Generate'.
+
+## Technology Used:
+GitHub Repository, 
+GitHub Projects,
+VS Code, 
+HTML, 
+CSS, 
+Formation-CSS, 
+JavaScript, 
+JQuery, 
+Google Chrome
+
+## Website Visual Demo(Gif/video):
+![Banner](Project_1-Demo-Gif.gif)
+https://drive.google.com/file/d/1B61MVkG7MmEzSRGXkcF_uf6C7CgrIDZA/view
+
+## Link to Deployed Page:
+https://tim-zebra.github.io/01_Student-Project/
+
+## References:
+Heart animation inspired by: codeachalesh - https://www.geeksforgeeks.org/pulsing-heart-animation-effect-using-html-css/ <br />
+CSS - html, body {} inspired by: Harshit Tewari - https://www.youtube.com/watch?v=OASSKoiCfqY <br />
+Checkboxes inspired by: jQuery UI - https://jqueryui.com/checkboxradio/
+
+## Development Process:
+Submitted project proposal <br />
+Setup and implemented a Kanban board <br />
+Created/Setup group github Repo <br />
+Wireframed HTML <br />
+Wireframed JS <br />
+Created HTML layout <br />
+Created JS to script page interaction <br />
+Applied Formation-CSS to web design <br />
+Quality Assurance
---
+++ assets/js/script.js
@@ -0,0 +1,375 @@
+// Global variables
+var searchHistoryArray = [];
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Form Submission//////////////////////////////////////
+// Sets variable for Form Submissions
+var compatibilityFormEl = $('#compatibilityForm');
+var firstNameEl = $('#firstName');
+var secondNameEl = $('#secondName');
+var compatibilitySectionEl = $('#compatibilitySection');
+var jokeCheckBoxSubmitEl = $('#jokeCheckBoxSubmit');
+
+// Gets input from user
+function getNamesInput (event) {
+    event.preventDefault();
+	// defined variables
+    var obj = {};
+    var firstName = '';
+	var secondName = '';
+
+    // Catches if no names are input. If no names, then do nothing
+    if ((firstNameEl.val() !== '') && (secondNameEl.val() !== '')) {
+        // Stores user search as an object
+        var firstName = firstNameEl.val().toLowerCase();
+        var secondName = secondNameEl.val().toLowerCase();
+
+        // Capitalizes first letter of name
+		firstName = capitalizeFirstLetter(firstName);
+		secondName = capitalizeFirstLetter(secondName);
+    }
+
+    obj.name1 = firstName;
+    obj.name2 = secondName;
+
+    return obj;
+}
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Joke Section//////////////////////////////////////////////////
+// Gets check box results and returns an object. Key = name of box: Value = boolean
+function getJokeCriteriaInput () {
+    // Assigns variables for categories of jokes
+    var jokeProgrammingBoxCheckEl = document.getElementById('jokeProgrammingBox');
+    var jokeMiscBoxCheckEl = document.getElementById('jokeMiscBox');
+    var jokeDarkBoxCheckEl = document.getElementById('jokeDarkBox');
+    var jokePunBoxCheckEl = document.getElementById('jokePunBox');
+    var jokeSpookyBoxCheckEl = document.getElementById('jokeSpookyBox');
+    var jokeChristmasBoxCheckEl = document.getElementById('jokeChristmasBox');
+
+    var arrayBoxes = [jokeProgrammingBoxCheckEl, jokeMiscBoxCheckEl, jokeDarkBoxCheckEl, jokePunBoxCheckEl, jokeSpookyBoxCheckEl, jokeChristmasBoxCheckEl];
+    var storedObject = {};
+    
+    // Checks if checked or not and creates an object
+    for (var k = 0; k < arrayBoxes.length; k++) {
+        var name = arrayBoxes[k].name;
+        if (arrayBoxes[k].checked === true) {
+            storedObject[name] = true;
+        }
+        else {
+            storedObject[name] = false;
+        }
+    }
+    return storedObject;
+}
+
+// Takes in a single string of letters and capatalizes the first letter
+function capitalizeFirstLetter (word) {
+	var newWord = '';
+	var arr = word.split('');
+
+	arr[0] = arr[0].toUpperCase();
+	newWord = arr.join('');
+
+	return newWord;
+}
+
+// Gets joke criteria, gets joke fetch, displays joke
+async function getJoke () {
+	var jokeSectionJokeEl = $('#jokeSectionJoke');
+	var searchCriteria = '';
+
+    // Gets joke parameters
+    var checkedOptions = getJokeCriteriaInput ();
+    var objKeys = Object.keys(checkedOptions);
+    
+    // Checks is any parameters were selected, if not then just searches all ('any')
+    for (var i = 0; i < objKeys.length; i++) {
+        if (checkedOptions[objKeys[i]] === true) {
+            searchCriteria += objKeys[i] + ',';
+        }
+    }
+
+    // Removes last ',' from string
+    searchCriteria = searchCriteria.slice(0, -1);    
+
+    // Prevents error if someone doesn't check any boxes
+    if (searchCriteria === '') {
+        searchCriteria = 'programming,spooky,christmas';
+    }
+
+    // Gets jokes from API
+    var onePartJoke = await fetchJoke (searchCriteria, '&type=single');
+    var twoPartJoke = await fetchJoke (searchCriteria, '&type=twopart');
+
+    // Removes any jokes already inplace. Positioned here to make replace appear 'seamless'
+    jokeSectionJokeEl.empty();
+    
+    // Catches joke if undefined
+    if (onePartJoke.joke === undefined) {
+        onePartJoke.joke = "No options for a witty one-linear. Try a different search criteria."
+    }
+
+    // Sets Joke Variables
+    onePartJoke = onePartJoke.joke;
+    twoPartJokeSetup = twoPartJoke.setup;
+    twoPartJokeDelivery = twoPartJoke.delivery;
+
+    // Displays joke
+    displayJoke (onePartJoke, twoPartJokeSetup, twoPartJokeDelivery);
+}
+
+function displayJoke(onePart, twoPartSet, twoPartDel) {
+    var jokeSectionJokeEl = $('#jokeSectionJoke');
+    
+    // Appends top border visual in HTML after search
+    var span = $('<span>');
+    jokeSectionJokeEl.append(span);
+
+    // Creates first Joke Section
+    var divOne = $('<div></div>');
+    var headOne = $('<h3>Witty Joke: </h3>');
+    var paraOne = $('<p>' + onePart + '</p>');
+
+    // Appends first joke
+    divOne.append(headOne);
+    divOne.append(paraOne);
+    jokeSectionJokeEl.append(divOne);
+
+    // Creates second joke
+    var divTwo = $('<div></div>');
+    var headTwo = $('<h3>Interactive Joke: </h3>');
+    var paraTwo = $('<p>' + twoPartSet + '</p>' + '\n' + '<p>' + twoPartDel + '</p>');
+
+    // Appends second joke
+    divTwo.append(headTwo);
+    divTwo.append(paraTwo);
+    jokeSectionJokeEl.append(divTwo);
+}
+
+// Listens for button submit on checkboxes
+jokeCheckBoxSubmitEl.on('click', getJoke);
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Compatibility Seciton//////////////////////////////////
+// Returns compatibility as an object with both names and a score
+async function createCompatibilityObj (event) {
+    var obj = getNamesInput(event);
+
+    // Calls function that reaches out to the API
+    obj.score = await fetchLove(obj.name1, obj.name2);
+    
+    saveToLocalStorage(obj);
+    return obj;
+}
+
+// Returns compatibility 
+async function compatibility (event) {
+    event.preventDefault();
+    var obj = await createCompatibilityObj(event);
+    console.log(obj);
+
+    // Adds progress to HMTL
+    const myProgressBar = document.querySelector(".progress");
+    updateProgressBar(myProgressBar, obj.score);
+    interpretCompatibilityScore(obj, obj.score);
+    
+    // Displays search history
+    displayHistory();
+}
+
+// Updates progress bar
+function updateProgressBar(progressBar, value) {
+	progressBar.querySelector(".progress-fill").style.width = `${value}%`;
+	progressBar.querySelector(".progress-text").textContent = `${value}%`;
+}
+
+// Interprets compatibility
+function interpretCompatibilityScore(names, score) {
+    var compatEl = $('#compat-interp');
+    var text = '';
+
+    // Interprets score
+    if (score <= 33) {
+        text = 'Not even a good joke can save you now. Search for another name. If you disagree, maybe try a middle name?'
+    } else if (score <= 66) {
+        text = 'This might be a match! Be sure to checkout our joke and activity section if you want things to go well.'
+    } else if (score <= 85) {
+        text = 'This is a good match! You\'ll defintitely want to have a good joke prepared or have a fun activity planned.'
+    } else {
+        text = 'This match has superb potential! Just don\'t mess it up. Always take a back up like a good joke or an alternative activity (see sections below).'
+    }
+
+    // Prompts to enter names if names are not entered.
+    if (names.name1 === "" || names.name2 === "") {
+        text = 'Please enter a name into both hearts.'
+    }
+
+    // Sets result on HTML
+    compatEl.text(text);
+}
+
+// Listens for the compatibility submit button
+compatibilityFormEl.on('submit', compatibility);
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ACITIVTY SECTION/////////////////////////////////////
+// Sets variables for activity section
+var activityGeneratorSubmitEl = $('#activityGeneratorSubmit');
+
+// Gets activity from fetch
+async function activity () {
+    var activity = await fetchActivity();
+    displayActivity (activity);
+}
+
+// Displays an activity
+function displayActivity (activity) {
+    var activityGeneratorEl = $('#activityGenerator');
+    var para = $('<p>' + activity + '</p>');
+
+    // Removes any previous activities
+    activityGeneratorEl.empty();
+
+    // Adds current activity
+    activityGeneratorEl.append(para);
+}
+
+// Listens for the activity submit button to be clicked
+activityGeneratorSubmitEl.on('click', activity);
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\API FETCHING/////////////////////////////////////////
+// // Love Compatability API
+async function fetchLove (name1, name2) {
+    var obj = {};
+    
+    const optionsLove = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com',
+            'X-RapidAPI-Key': 'd7b52d453cmsh879a19108989e7bp13154cjsn9c9ddd1cbe5d'
+        }
+    };
+    await fetch('https://love-calculator.p.rapidapi.com/getPercentage?sname=' + name1 + '&fname=' + name2, optionsLove)
+    .then(response => response.json())
+    .then(function (data) {
+        obj = data;
+    })
+    .catch(err => console.error(err));
+
+    return obj.percentage;
+}
+
+// Joke API
+async function fetchJoke (criteria, parts) {
+	var fetchedObj = {};
+
+    const jokeAPI = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'https://v2.jokeapi.dev/',
+			'X-RapidAPI-Key': ''
+		}
+	};
+
+	await fetch('https://v2.jokeapi.dev/joke/' + criteria + '?blacklistFlags=nsfw,religious,political,racist,sexist,explicit' + parts, jokeAPI)
+		.then(response => response.json())
+		.then(function (data) {
+            fetchedObj = data;
+		})
+		.catch(err => console.error(err));
+
+    return fetchedObj;
+}
+
+// Pulls up a random Activity for two people
+async function fetchActivity () {
+    var obj = {};
+    
+    await fetch('https://www.boredapi.com/api/activity')
+	.then(response => response.json())
+	.then(function (data){
+		obj = data;
+	})
+	.catch(err => console.error(err));
+
+    return obj.activity;
+}
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\Save, Load Local Storage////////////////////////////
+// Save to local storage
+function saveToLocalStorage(obj) {
+    // Avoids null from empty search history array and avoids pushing undefined object
+    if (searchHistoryArray[0] === null) {
+        searchHistoryArray[0] = obj;
+    } else if (obj !== undefined) {
+        searchHistoryArray.unshift(obj);
+    }
+
+    localStorage.setItem('Lovers', JSON.stringify(searchHistoryArray));
+}
+
+// Load from local storage
+function getFromLocalStorage() {
+   var getFromStorage = JSON.parse(localStorage.getItem('Lovers'));
+   if (getFromStorage !== null) {
+        searchHistoryArray = getFromStorage;
+    }
+}
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\Display Search History List///////////////////////////
+//Displays search history in HTML
+async function displayHistory() {
+    var list = $('#listHistory');
+    // Removes any content in search history
+    list.empty();
+
+    if (searchHistoryArray !== undefined) {
+        for (var i = 0; i < searchHistoryArray.length; i++) {
+            // Gets value from object array
+            var name1 = searchHistoryArray[i].name1;
+            var name2 = searchHistoryArray[i].name2;
+            var compat = searchHistoryArray[i].score;
+            
+            // Creates li value for HTML
+            var addTo = document.createElement('li');
+            addTo.textContent = name1 + ' and ' + name2;
+
+            // Creates progress bar for HTML
+            var divProg = document.createElement("div");
+            divProg.setAttribute('class', 'progress');
+            divProg.setAttribute('id', 'progress' + i)
+            
+            var divProgFill = document.createElement("div");
+            divProgFill.setAttribute('class', 'progress-fill');
+
+            var spanProg = document.createElement("span");
+            spanProg.setAttribute('class', 'progress-text');
+
+            divProg.appendChild(divProgFill);
+            divProg.appendChild(spanProg);            
+
+            addTo.append(divProg);
+            list.append(addTo);
+
+            // Applies fill to progress bar
+            var progressBar = document.getElementById('progress' + i);
+            updateProgressBar(progressBar, compat);
+        }
+    }
+}
+
+// Clears search history list
+function clearSearchHistory () {
+    // Clears all varibales
+    searchHistoryArray = [];
+
+    var listHistory = $('#listHistory');
+    listHistory.empty();
+
+    // Saves to local storage
+    saveToLocalStorage();
+}
+
+// Button to clear search history
+var clearSearchHistoryBtn = $('#clearSearchHistory');
+clearSearchHistoryBtn.on('click', clearSearchHistory);
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\initaites base app/////////////////////////////////
+function init() {
+    // Loads data from storage
+    getFromLocalStorage();
+    
+    // Display search history
+    displayHistory();
+}
+init();
